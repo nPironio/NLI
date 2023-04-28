d12facc0a154ebaab42c6602fef37c36e9c8b2a8
nicop <nicopironio@gmail.com>
2023-04-27 23:00:48 -0300

    Add mlflow logging

--- scripts/fit_w2v.py
+++ scripts/fit_w2v.py
@@ -1,3 +1,4 @@
+import mlflow
 import numpy as np
 import pandas as pd
 import torch
@@ -18,31 +19,44 @@ if __name__ == '__main__':
     train = pd.read_parquet('../data/train.parquet')
     val = pd.read_parquet('../data/dev.parquet')
 
-    train_sentences = sample_sentences(train, 1000)
-    val_sentences = sample_sentences(val, 500)
+    train_sentences = 10000
+    val_sentences = 3000
+
+    train_sentences = sample_sentences(train, train_sentences)
+    val_sentences = sample_sentences(val, val_sentences)
 
     train_ds = W2Vdataset(data=train_sentences, tokenizer=tokenizer)
     val_ds = W2Vdataset(data=val_sentences, tokenizer=tokenizer)
 
+    embedding_size = 100
+    hidden_size = 256
     lr = 1e-3
-    batch_size = 32
+    batch_size = 128
     epochs = 10
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
     loss_fn = torch.nn.BCELoss()
+
     model = Word2Vec(tokenizer.get_vocab_size(), embedding_size=100, hidden_size=100).to(device)
     optimizer = torch.optim.Adam(params=model.parameters(), lr=lr)
 
     train_dl = DataLoader(train_ds, shuffle=True, batch_size=batch_size)
     val_dl = DataLoader(val_ds, shuffle=True, batch_size=batch_size)
 
-    train_epoch_losses = []
-    val_epoch_losses = []
-
+    mlflow.set_tracking_uri('../mlruns')
+    mlflow.set_experiment('fit_w2v')
+    mlflow.log_params(
+        {
+            "emb_size": embedding_size, "hidden_size": hidden_size, "lr": lr,
+            "batch_size": batch_size, "epochs": epochs,
+            "train_size": len(train_ds), "val_size": len(val_ds)
+         }
+    )
+    train_epoch_loss = 0
+    val_epoch_loss = 0
     try:
         for epoch in range(epochs):
             losses = []
-            print(f'{epoch=}\n --------------------')
+            print(f'-------------------- {epoch=} --------------------')
             for ix, batch in enumerate(train_dl):
                 data, target = batch
                 preds = model(data.to(device))
@@ -52,8 +66,9 @@ if __name__ == '__main__':
                 loss.backward()
                 optimizer.step()
 
+                mlflow.log_metric('train_step', loss.item())
                 losses.append(loss.item())
-            train_epoch_losses.append(np.mean(losses))
+            train_epoch_loss = np.mean(losses)
 
             with torch.no_grad():
                 losses = []
@@ -62,12 +77,12 @@ if __name__ == '__main__':
                     preds = model(data.to(device))
                     loss = loss_fn(preds, target.to(device))
 
+                    mlflow.log_metric('val_step', loss.item())
                     losses.append(loss.item())
 
-            val_epoch_losses.append(np.mean(losses))
+            val_epoch_loss = np.mean(losses)
 
-            print(f'    train loss: {train_epoch_losses[-1]}')
-            print(f'    val loss: {val_epoch_losses[-1]}')
+            mlflow.log_metrics({"train_epoch_loss": train_epoch_loss, "val_epoch_loss": val_epoch_loss})
 
     except KeyboardInterrupt:
         pass
