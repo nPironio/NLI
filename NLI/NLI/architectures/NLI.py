4fffb4aa4a3d04c16d297e3ba97f5d1bad7fc5f8
nicop <nicopironio@gmail.com>
2023-05-07 13:01:08 -0300

    Refactor and mask sentence before forward

--- NLI/NLI/architectures/NLI.py
+++ NLI/NLI/architectures/NLI.py
@@ -6,20 +6,22 @@ class CrossAttention(torch.nn.Module):
     def __init__(self, emb_size: int = 100, hidden_size: int = 200, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.layer = torch.nn.Linear(emb_size, hidden_size)
-        self.activation = torch.nn.ReLU()
+        self.transform = torch.nn.Sequential(
+            torch.nn.Linear(emb_size, hidden_size),
+            torch.nn.ReLU()
+        )
 
-    def forward(self, v, w, v_mask, w_mask):
-        t_v = self.activation(self.layer(v))
-        t_w = self.activation(self.layer(w))
+    def forward(self, v, w):
+        t_v = self.transform(v)
+        t_w = self.transform(w)
 
         sim = torch.bmm(t_v, torch.transpose(t_w, 1, 2))
 
         alpha_v = softmax(sim, dim=2)
         alpha_w = torch.transpose(softmax(sim, dim=1), 1, 2)
 
-        w_alignment = torch.bmm(alpha_v, w * w_mask.unsqueeze(-1))
-        v_alignment = torch.bmm(alpha_w, v * v_mask.unsqueeze(-1))
+        w_alignment = torch.bmm(alpha_v, w)
+        v_alignment = torch.bmm(alpha_w, v)
 
         return w_alignment, v_alignment
 
@@ -55,9 +57,12 @@ class DecomposableAttentionNetwork(torch.nn.Module):
         self.out = OutLayer(compare_hidden)
 
     def forward(self, v, w, v_mask, w_mask):
-        w_alignment, v_alignment = self.attention(v, w, v_mask, w_mask)
+        masked_v = v * v_mask.unsqueeze(-1)
+        masked_w = w * w_mask.unsqueeze(-1)
 
-        z_v = self.compare(v, w_alignment)
-        z_w = self.compare(w, v_alignment)
+        w_alignment, v_alignment = self.attention(masked_v, masked_w)
+
+        z_v = self.compare(masked_v, w_alignment)
+        z_w = self.compare(masked_w, v_alignment)
 
         return self.out(z_v, z_w)
